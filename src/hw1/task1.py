4894f183a8f5366dd96044a7921968405deb9d4e
Arseniy Volynets <vol0ncar@yandex.ru>
2022-02-24 13:14:55 +0300

    First version

---
+++ src/hw1/task1.py
@@ -0,0 +1,75 @@
+from math import sqrt
+from copy import deepcopy
+from typing import Union
+
+Num = Union[int, float, complex]
+Vector = list[Num]
+Matrix = list[list[Num]]
+
+
+def check_vectors_compatible(vec1: Vector, vec2: Vector):
+    if len(vec1) != len(vec2):
+        raise ValueError("Vectors should have the same size!")
+
+
+def calc_product(vec1: Vector, vec2: Vector) -> Num:
+    check_vectors_compatible(vec1, vec2)
+    return sum(map(lambda x, y: x * y, vec1, vec2))
+
+
+def calc_len(vec: Vector) -> Num:
+    if not vec:
+        raise ValueError("Vector should not be empty to calc his len!")
+    return sqrt(sum(map(lambda x: x ** 2, vec)))
+
+
+# actually it return the cos of the angle
+def calc_angle(vec1: Vector, vec2: Vector) -> Num:
+    check_vectors_compatible(vec1, vec2)
+    len1, len2 = calc_len(vec1), calc_len(vec2)
+    # if one of the vectors is null, then angle is zero
+    if not len1 * len2:
+        return 1
+    return calc_product(vec1, vec2) / (len1*len2)
+
+
+def check_is_matrix(mat: Matrix):
+    if len(mat) and not mat:
+        return
+    assert len(mat), "Minimal matrix is [[]], not []"
+    if not all(map(lambda row: len(row) == len(mat[0]), mat)):
+        raise ValueError('All rows of a matrix must have the same size.')
+
+
+def check_matrices_equal_size(mat1: Matrix, mat2: Matrix):
+    for mat in (mat1, mat2):
+        check_is_matrix(mat)
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        raise ValueError('Matrices must have the same size.')
+
+
+def two_add(mat1: Matrix, mat2: Matrix):
+    check_matrices_equal_size(mat1, mat2)
+    for i in range(len(mat1)):
+        for j in range(len(mat1[0])):
+            mat1[i][j] += mat2[i][j]
+
+
+def add(mat1: Matrix, *args: Matrix) -> Matrix:
+    res = deepcopy(mat1)
+    for arg in args:
+        two_add(res, arg)
+    return res
+
+
+def transpose(mat: Matrix) -> Matrix:
+    return [[mat[i][j] for i in range(len(mat))] for j in range(len(mat[0]))]
+
+
+def mul(mat1: Matrix, mat2: Matrix) -> Matrix:
+    for mat in (mat1, mat2):
+        check_is_matrix(mat)
+    assert mat1 and mat2, "Can't multiply empty matrices!"
+    if len(mat1[0]) != len(mat2):
+        raise ValueError(f'Wrong sizes for multiplication: {(len(mat1), len(mat1[0]))}, {(len(mat2), len(mat2[0]))}')
+    return [[calc_product(row, col) for col in zip(*mat2)] for row in mat1]
---
+++ tests/hw1/test_task1.py
@@ -0,0 +1,172 @@
+import pytest
+import src.hw1.task1 as t1
+import math
+
+epsilon = 1e-5
+
+
+def check_real_equal(a, b):
+    return abs(b - a) < epsilon
+
+
+@pytest.mark.parametrize("vec1,vec2", [([], []), ([1], [1]), ([1, 3], [2, 2])])
+def test_check_vectors_compatible_result(vec1, vec2):
+    t1.check_vectors_compatible(vec1, vec2)
+
+
+@pytest.mark.parametrize("vec1,vec2,exception", [([], [1], ValueError), ([1], [1, 2], ValueError)])
+def test_check_vectors_compatible_exception(vec1, vec2, exception):
+    with pytest.raises(exception):
+        t1.check_vectors_compatible(vec1, vec2)
+
+
+@pytest.mark.parametrize("vec1,vec2,expected",
+                         [([1], [1], 1), ([1, 3], [1, 3], 10), ([1j, 1j], [1, -1], 0), ([], [], 0)])
+def test_calc_product_result(vec1, vec2, expected):
+    assert t1.calc_product(vec1, vec2) == expected
+
+
+@pytest.mark.parametrize("vec1,vec2,exception", [([1, 2], [1], ValueError)])
+def test_calc_product_raises(vec1, vec2, exception):
+    with pytest.raises(exception):
+        t1.calc_product(vec1, vec2)
+
+
+@pytest.mark.parametrize("vec,expected", [([1], 1), ([4, 3], 5), ([0, 0, 0], 0), ([0], 0), ])
+def test_calc_len(vec, expected):
+    assert t1.calc_len(vec) == expected
+
+
+@pytest.mark.parametrize("vec,exception", [([], ValueError)])
+def test_calc_len_raises(vec, exception):
+    with pytest.raises(exception):
+        t1.calc_len(vec)
+
+
+@pytest.mark.parametrize("vec1,vec2,expected",
+                         [([1, 1], [1, 1], 1), ([0, 1], [1, 0], 0), ([1, 0], [1, math.sqrt(3)], 1 / 2)])
+def test_calc_angle_result(vec1, vec2, expected):
+    assert check_real_equal(t1.calc_angle(vec1, vec2), expected)
+
+
+@pytest.mark.parametrize("vec1,vec2,exception",
+                         [([], [1], ValueError), ([], [], ValueError)])
+def test_calc_angle_raises(vec1, vec2, exception):
+    with pytest.raises(exception):
+        t1.calc_angle(vec1, vec2)
+
+
+@pytest.mark.parametrize("mat", [([[1]]), ([[1], [2]]), ([[1, 2, 3], [1j, 1 + 2j, 1]]), ([[]])])
+def test_check_is_matrix_not_raises(mat):
+    t1.check_is_matrix(mat)
+
+
+@pytest.mark.parametrize("mat,exception", [([[1], [2, 3]], ValueError), ([[1], []], ValueError), ([1], Exception), ([], Exception)])
+def test_check_is_matrix_raises(mat, exception):
+    with pytest.raises(exception):
+        t1.check_is_matrix(mat)
+
+
+@pytest.mark.parametrize("mat1,mat2",
+                         [([[1]], [['x']]), ([[1], [2]], [[3], [4]]),
+                          ([[1, 1, 1],
+                            [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]), ([[]], [[]])])
+def test_check_matrices_equal_size_not_raises(mat1, mat2):
+    t1.check_matrices_equal_size(mat1, mat2)
+
+
+@pytest.mark.parametrize("mat1,mat2,exception",
+                         [([[1]], [['x', 's']], ValueError), ([[], []], [[]], ValueError)])
+def test_check_matrices_equal_size_not_raises(mat1, mat2, exception):
+    with pytest.raises(exception):
+        t1.check_matrices_equal_size(mat1, mat2)
+
+
+@pytest.mark.parametrize("mat1,mat2,expected",
+[([[1, 2],
+  [1, 1]],
+
+  [[2, 1],
+   [2, 2]],
+
+  [[3, 3],
+   [3, 3]]),
+ ############
+     ([[1]],
+      [[1j]],
+      [[1+1j]]),
+ #############
+ ([[]], [[]], [[]])
+])
+def test_two_add_result(mat1, mat2, expected):
+    t1.two_add(mat1, mat2)
+    assert mat1 == expected
+
+
+@pytest.mark.parametrize("mat1,mat2,mat3,expected",
+                         [([[1, 2],
+                            [1, 1]],
+
+                           [[2, 1],
+                            [2, 2]],
+
+                           [[3, 3],
+                            [3, 3]],
+                           [[6, 6],
+                            [6, 6]]),
+                          ############
+                          ([[1]],
+                           [[1]],
+                           [[1]],[[3]]),
+                          ])
+def test_add_result(mat1, mat2, mat3, expected):
+    assert t1.add(mat1, mat2, mat3) == expected
+
+
+@pytest.mark.parametrize("mat,expected",
+                         [([[1, 2],
+                            [1, 1]],
+
+                           [[1, 1],
+                            [2, 1]]),
+
+                          ([[3, 3, 3],
+                            [1, 1, 1]],
+                           [[3, 1],
+                            [3, 1],
+                            [3, 1]]),
+                          ])
+def test_transpose_result(mat, expected):
+    assert t1.transpose(mat) == expected
+
+
+@pytest.mark.parametrize("mat1,mat2,expected",
+                         [([[1, 0],
+                            [0, 1]],
+
+                           [[2, 1],
+                            [2, 2]],
+
+                           [[2, 1],
+                            [2, 2]]),
+                          ############
+                          ([[1, 0, 1],
+                            [0, 1, 0]],
+
+                           [[2, 2],
+                            [2, 2],
+                            [2, 2]],
+
+                           [[4, 4],
+                            [2, 2]]),
+                          ############
+                          ])
+def test_mul_result(mat1, mat2, expected):
+    assert t1.mul(mat1, mat2) == expected
+
+
+@pytest.mark.parametrize("mat1,mat2,exception",
+                             [([[]], [[]], ValueError), ([[]], [[1, 2]], ValueError)])
+def test_mul_raises(mat1, mat2, exception):
+    with pytest.raises(exception):
+        t1.mul(mat1, mat2)
