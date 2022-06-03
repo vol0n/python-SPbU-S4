7ea032d27b51f7161c39ec9d44d818c84abe3b00
Arseniy Volynets <vol0ncar@yandex.ru>
2022-06-03 04:37:03 +0300

    remove not working params

--- src/final/bot.py
+++ src/final/bot.py
@@ -1,6 +1,5 @@
 import logging
 import os
-from math import inf
 from telegram import ForceReply, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext, MessageHandler, filters
 from transformers import pipeline
@@ -9,8 +8,6 @@ logging.basicConfig(format="%(asctime)s | %(name)s | %(levelname)s : %(message)s
 logger = logging.getLogger(__name__)
 
 generator = pipeline("text-generation", model="sshleifer/tiny-gpt2")
-max_len = 30
-num_of_sentences = 1
 
 
 async def start(update: Update, context: CallbackContext.DEFAULT_TYPE) -> None:
@@ -30,8 +27,6 @@ async def help_command(update: Update, context: CallbackContext.DEFAULT_TYPE) ->
         return None
     await message.reply_text(
         "This bot generates some text from your text using gp2 model!\n"
-        + "You can set the length of the generated text using \\len command."
-        "You can set the number of the generated text using \\num command."
     )
 
 
@@ -42,40 +37,10 @@ async def generate(update: Update, context: CallbackContext.DEFAULT_TYPE) -> Non
     generated_text = "\n".join(
         data["generated_text"]
         for data in generator(
-            update.message.text, max_length=max_len + len(update.message.text), num_return_sequences=num_of_sentences
+            update.message.text
         )
     )
     await update.message.reply_text(generated_text)
-
-
-def check_str_to_int(value: str, left: int = 1, right=inf) -> bool:
-    try:
-        converted: int = int(value)
-        return left <= converted < right
-    except ValueError:
-        return False
-
-
-async def set_num(update: Update, context: CallbackContext.DEFAULT_TYPE) -> None:
-    global num_of_sentences
-    if context.args is None or update.message is None:
-        return
-    if check_str_to_int(context.args[0]):
-        num_of_sentences = int(context.args[0])
-        await update.message.reply_text(f"Set number of generated sentences to {num_of_sentences}")
-    else:
-        await update.message.reply_text("Please provide a positive number!")
-
-
-async def set_len(update: Update, context: CallbackContext.DEFAULT_TYPE) -> None:
-    global max_len
-    if context.args is None or update.message is None:
-        return
-    if check_str_to_int(context.args[0]):
-        max_len = int(context.args[0])
-        await update.message.reply_text(f"Set length of the generated message to {max_len}")
-    else:
-        await update.message.reply_text("Please provide a positive number!")
 
 
 def main() -> None:
@@ -83,7 +48,6 @@ def main() -> None:
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("len", set_len))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, generate))
 
     application.run_polling()
